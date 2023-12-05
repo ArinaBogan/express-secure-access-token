99e8ae4d7671d57fe3799388371bc2219ea0d26d
ArinaBogan <arina.bogan@gmail.com>
2023-12-05 14:16:23 +0300

    add task

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { createUser,authUser } = require('../service/user.service')
-const buildResponse = require('../helper/buildResponse')
+const { createUser, authUser } = require('../service/user.service');
+const buildResponse = require('../helper/buildResponse');
+const createToken = require('../helper/jwt');
 
 const route = express.Router();
 
@@ -18,6 +19,8 @@ route.post('/auth', async (req, res) => {
     try {
         const { email, pwd } = req.body;
         const data = await authUser(email, pwd);
+        const token = createToken(data);
+        res.setHeader('authorization', [token])
         buildResponse(res, 200, data);
     } catch (error) {
         buildResponse(res, 404, error.message)
